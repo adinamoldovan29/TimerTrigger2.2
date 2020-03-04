31e09cf03c42446c738689f707e30989d8b7fc6c
Adina Moldovan <adina.moldovan@vizolution.co.uk>
2020-03-04 10:19:18 +0200

    Add function with blob container trigger

---
+++ FunctionApp1/BlobContainerTriggerFunction.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionApp1
+{
+    public static class BlobContainerTriggerFunction
+    {
+        [FunctionName("BlobContainerTriggerFunction")]
+        public static void Run([BlobTrigger("container04032020/{name}", Connection = "")]Stream myBlob, string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+        }
+    }
+}
