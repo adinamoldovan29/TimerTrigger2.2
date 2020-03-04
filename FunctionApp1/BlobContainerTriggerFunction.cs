5ad07b802781544315852f742dc64c1f0dace16f
Adina Moldovan <adina.moldovan@vizolution.co.uk>
2020-03-04 10:59:28 +0200

    Add connection stirng to blob function

--- FunctionApp1/BlobContainerTriggerFunction.cs
+++ FunctionApp1/BlobContainerTriggerFunction.cs
@@ -9,7 +9,8 @@ namespace FunctionApp1
     public static class BlobContainerTriggerFunction
     {
         [FunctionName("BlobContainerTriggerFunction")]
-        public static void Run([BlobTrigger("container04032020/{name}", Connection = "")]Stream myBlob, string name, ILogger log)
+        public static void Run([BlobTrigger("container04032020/{name}", 
+            Connection = "DefaultEndpointsProtocol=https;AccountName=storeageaccount04032020;AccountKey=SrJxUF0AQWOP9ICBxQtanr68mn/CRnbu23oKQE1kRILqu/W6FbqZCr7veZGXFlWYRXScyGj7QTMebCgYXcd2dw==;EndpointSuffix=core.windows.net")]Stream myBlob, string name, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
         }
