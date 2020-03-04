bb133787b97cc14897bc638da077f334243f8e36
Adina Moldovan <adina.moldovan@vizolution.co.uk>
2020-03-04 16:55:58 +0200

    Change connection string

--- FunctionApp1/BlobContainerTriggerFunction.cs
+++ FunctionApp1/BlobContainerTriggerFunction.cs
@@ -10,7 +10,7 @@ namespace FunctionApp1
     {
         [FunctionName("BlobContainerTriggerFunction")]
         public static void Run([BlobTrigger("container04032020/{name}", 
-            Connection = "DefaultEndpointsProtocol=https;AccountName=storeageaccount04032020;AccountKey=SrJxUF0AQWOP9ICBxQtanr68mn/CRnbu23oKQE1kRILqu/W6FbqZCr7veZGXFlWYRXScyGj7QTMebCgYXcd2dw==;EndpointSuffix=core.windows.net")]Stream myBlob, string name, ILogger log)
+            Connection = "BlobStorageConnString")]Stream myBlob, string name, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
         }
